c0cde8dc9636096f14617b377f37c39c541356b3
JoseDEV799 <josedev799@gmail.com>
2024-10-06 19:30:04 -0500

    vercel config v1.1.8

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import app from "../src/app.js"
-import { conectDB } from "./db.js"
+import { conectDB } from "../src/db.js"
 conectDB()
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
