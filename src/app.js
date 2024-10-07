44092d34a6ab072913beff465ea2f356917af5ae
JoseDEV799 <josedev799@gmail.com>
2024-10-07 01:46:25 -0500

    vercel config v1.1.20

--- src/app.js
+++ src/app.js
@@ -16,11 +16,12 @@ const app = express();
 app.use(
     cors({
         origin: 'https://jose-dev-799-biblioteca-ie-22455-pisco-frontend.vercel.app',
-        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+        // methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
         credentials: true,
         // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
     })
 );
+// app.options('*', cors());
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
