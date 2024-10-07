cb8508a0122d4666ee549a4b81ef71f056c71b89
JoseDEV799 <josedev799@gmail.com>
2024-10-06 22:34:09 -0500

    vercel config v1.1.15

--- src/app.js
+++ src/app.js
@@ -18,49 +18,12 @@ const corsOptions = {
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 };
+app.options('*', cors(corsOptions)); // Aquí está el cambio
 
 app.use(cors(corsOptions));
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
-
-app.get('/api/fake-books', async (req, res) => {
-    const categories = await Category.find();
-    const fakeBooks = [];
-    for (let i = 0; i < 50; i++) {
-        const randomCategory = faker.helpers.arrayElement(categories.map(category => category.name))
-        fakeBooks.push({
-            title: faker.word.words(5),
-            author: faker.person.fullName(),
-            category: randomCategory,
-            stock: String(faker.number.bigInt({ min: 1n, max: 9n })),
-            observation: faker.string.fromCharacters(['O', 'C'])
-        });
-    }
-    try {
-        await Book.insertMany(fakeBooks); // Usa await para la operación
-        res.status(201).json({ message: 'Categorías falsas generadas', categories: fakeBooks });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al insertar categorías', error: error.message });
-    }
-});
-
-app.get('/api/fake-categories', async (req, res) => {
-    const fakeCategories = [];
-    for (let i = 0; i < 20; i++) {
-        fakeCategories.push({
-            name: faker.word.words(1),
-            abbreviation: faker.string.fromCharacters(['A', 'B'])
-        });
-    }
-    try {
-        await Category.insertMany(fakeCategories); // Usa await para la operación
-        res.status(201).json({ message: 'Categorías falsas generadas', categories: fakeCategories });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al insertar categorías', error: error.message });
-    }
-});
-
 
 app.use('/api', authRoutes)
 app.use('/api', bookRoutes)
