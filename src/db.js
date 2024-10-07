dce76da7253a21bb3c0a24d1723bd8ddf0c6b7af
JoseDEV799 <josedev799@gmail.com>
2024-10-07 01:02:38 -0500

    vercel config v1.1.20

--- src/db.js
+++ src/db.js
@@ -4,9 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 export const conectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL,{
-            serverSelectionTimeoutMS: 15000,
-        })
+        await mongoose.connect(process.env.DATABASE_URL)
         console.log('Conectado a la base de datos')
     } catch (error) {
         console.log(error)
