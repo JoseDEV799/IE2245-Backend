2c62f6f0cef4f385a5cc6f70128671efaa366f82
JoseDEV799 <josedev799@gmail.com>
2024-10-06 22:59:02 -0500

    vercel config v1.1.16

--- src/app.js
+++ src/app.js
@@ -5,28 +5,20 @@ import cors from 'cors'
 import authRoutes from './routes/auth.routes.js'
 import bookRoutes from './routes/book.routes.js'
 import categoryRoutes from './routes/category.routes.js'
-import { faker } from '@faker-js/faker'
-import Category from './models/category.model.js'
-import Book from './models/book.model.js'
+// import {faker} from '@faker-js/faker'
+// import Category from './models/category.model.js'
+// import Book from './models/book.model.js'
 import dotenv from 'dotenv';
 
 dotenv.config()
 const app = express();
-const corsOptions = {
-    origin: 'https://jose-dev-799-biblioteca-ie-22455-pisco-frontend.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-};
-app.options('*', cors(corsOptions)); // Aquí está el cambio
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api', authRoutes)
-app.use('/api', bookRoutes)
-app.use('/api', categoryRoutes)
+app.use('/api',authRoutes)
+app.use('/api',bookRoutes)
+app.use('/api',categoryRoutes)
 
 export default app;
