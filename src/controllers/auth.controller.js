7da1d76bf0b0dac0eb61d6537b72a93c513bef68
JoseDEV799 <josedev799@gmail.com>
2024-10-09 13:02:21 -0500

    Restrict

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -49,7 +49,8 @@ export const login = async (req, res) => {
         const token = await createAccessToken({ id: userFound._id });
 
         // Establece la cookie manualmente usando setHeader
-        res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/; SameSite=Lax; Secure=${process.env.NODE_ENV === 'production'}`);
+        res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/; Max-Age=3600; SameSite=Lax; Secure=${process.env.NODE_ENV === 'production'}`);
+
 
         res.status(200).json({
             id: userFound._id,
