d6890fcd5378c93cbdfcbd78fb5621272e6d26cc
JoseDEV799 <josedev799@gmail.com>
2024-10-14 20:44:07 -0500

    Domain

--- src/db.js
+++ src/db.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config()
 export const conectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL)
+        // await mongoose.connect(process.env.DATABASE_URL)
+        await mongoose.connect("mongodb+srv://josedev799:HqYkNhH3CJ30jZvM@ie22455pisco.galew.mongodb.net")
         console.log('Conectado a la base de datos')
     } catch (error) {
         console.log(error)
