15fc0a8722dc15c187d981992bea59fd82a6cc18
JoseDEV799 <josedev799@gmail.com>
2024-10-14 20:44:59 -0500

    Domain

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-    origin: ['https://frontend-ie22455.vercel.app', 'http://localhost:5173'],
+    origin: ['https://biblioteca.josedev.net.pe', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
